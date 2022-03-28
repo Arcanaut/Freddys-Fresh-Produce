03e4a80a644de45a35cfd325d583573a830746e3
Brendan Ahearn <91020994+Arcanaut@users.noreply.github.com>
2022-03-27 20:13:17 -0500

    merged origin/Travis and added the npm package "graphql-upload"
    merged origin/Travis to resolve package.json and package-lock.json filesmerge also updated npms.
    
    FILE ADVISORY: fileUploadTutorial.js in server schema will later be deleted upon completion, will be divided and refactored to more appropriate file locations for clarity and efficiency.
    
    It is suggested to delete it in local branch as it may interfere with server functions.

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,9 +1,31 @@
+const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
+const {
+  GraphQLUpload,
+  graphqlUploadExpress, // A Koa implementation is also exported.
+} = require('graphql-upload');
+const { finished } = require('stream/promises');
+
 const resolvers = {
-    Query: {
-      helloWorld: () => {
-        return 'Hello world!';
-      }
-    }
-  };
-  
-  module.exports = resolvers;
+  // This maps the `Upload` scalar to the implementation provided
+  // by the `graphql-upload` package.
+  Upload: GraphQLUpload,
+
+  Mutation: {
+    singleUpload: async (parent, { file }) => {
+      const { createReadStream, filename, mimetype, encoding } = await file;
+
+      // Invoking the `createReadStream` will return a Readable Stream.
+      // See https://nodejs.org/api/stream.html#stream_readable_streams
+      const stream = createReadStream();
+
+      // This is purely for demonstration purposes and will overwrite the
+      // local-file-output.txt in the current working directory on EACH upload.
+      const out = require('fs').createWriteStream('local-file-output.txt');
+      stream.pipe(out);
+      await finished(out);
+
+      return { filename, mimetype, encoding };
+    },
+  },
+};
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,9 +1,32 @@
-const { gql } = require("apollo-server-express");
+const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
+const {
+  GraphQLUpload,
+  graphqlUploadExpress, // A Koa implementation is also exported.
+} = require('graphql-upload');
+const { finished } = require('stream/promises');
 
 const typeDefs = gql`
+  # The implementation for this scalar is provided by the
+  # 'GraphQLUpload' export from the 'graphql-upload' package
+  # in the resolver map below.
+  scalar Upload
+
+  type File {
+    filename: String!
+    mimetype: String!
+    encoding: String!
+  }
+
   type Query {
-    helloWorld: String
+    # This is only here to satisfy the requirement that at least one
+    # field be present within the 'Query' type.  This example does not
+    # demonstrate how to fetch uploads back.
+    otherFields: Boolean!
+  }
+
+  type Mutation {
+    # Multiple uploads are supported. See graphql-upload docs for details.
+    singleUpload(file: Upload!): File!
   }
 `;
-
-module.exports = typeDefs;
