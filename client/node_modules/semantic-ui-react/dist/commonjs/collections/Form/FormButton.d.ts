e547e880b1f1bc3bfe894804710e7f4a53090bc2
BoKok <kokbo@yahoo.com>
2022-03-28 14:50:59 -0500

    merge

---
+++ client/node_modules/semantic-ui-react/dist/commonjs/collections/Form/FormButton.d.ts
@@ -0,0 +1,27 @@
+import * as React from 'react'
+
+import { SemanticShorthandItem } from '../../generic'
+import { StrictButtonProps } from '../../elements/Button'
+import { LabelProps } from '../../elements/Label'
+import { StrictFormFieldProps } from './FormField'
+
+export interface FormButtonProps extends StrictFormButtonProps {
+  [key: string]: any
+}
+
+export interface StrictFormButtonProps
+  extends StrictFormFieldProps,
+    Omit<StrictButtonProps, 'type'> {
+  /** An element type to render as (string or function). */
+  as?: any
+
+  /** A FormField control prop. */
+  control?: any
+
+  /** Shorthand for a Label. */
+  label?: SemanticShorthandItem<LabelProps>
+}
+
+declare const FormButton: React.StatelessComponent<FormButtonProps>
+
+export default FormButton
