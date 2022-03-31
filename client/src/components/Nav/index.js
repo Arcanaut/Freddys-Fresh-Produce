cc892a990c5c8bbc88823cafd63c99a196d6e46a
Brendan Ahearn <91020994+Arcanaut@users.noreply.github.com>
2022-03-31 18:07:14 -0500

    Styled Signup page to be responsive, made progress on Nav Bar.
    //TODO: Change header to be responsive so it doesn't overlap with anything
    //TODO: Break the bar so that The links are underneath Freddy's name banner
    //TODO: style logged in nav to match logged out nav.
    //TODO: Possibly move Nav bars to left side as a sticky bar that travels down

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,38 +1,54 @@
 import React from "react";
 import Auth from "../../utils/auth";
-import { Link } from "react-router-dom";
-
+import { Link, NavLink } from "react-router-dom";
+import { Menu } from 'semantic-ui-react'
+//TODO: Change header to be responsive so it doesn't overlap with anything
+//TODO: Break the bar so that The links are underneath Freddy's name banner
+//TODO: style Logout and Order History?
 function Nav() {
   function showNavigation() {
     if (Auth.loggedIn()) {
       return (
         <ul className="flex-row">
-          <li className="mx-1">
-            <Link to="/orderHistory">Order History</Link>
-          </li>
+          <Menu>
+          <Menu.Item as={ NavLink } 
+            exact to="/orderHistory"> Order History </Menu.Item>
+            
+            
           <li className="mx-1">
             <a href="/" onClick={() => Auth.logout()}>
               Logout
             </a>
           </li>
+          </Menu>
         </ul>
       );
     } else {
       return (
         <ul className="flex-row">
-          <li className="mx-1">
-            <Link to="/signup">Join Fresh Freddy's</Link>
-          </li>
-          <li className="mx-1">
-            <Link to="/login">Login</Link>
-          </li>
+          <Menu>
+            <Menu.Item as={ NavLink } 
+            exact to="/signup">
+              Sign Up
+            </Menu.Item>
+
+              <Menu.Item as={ NavLink }
+              exact to="/login">
+            Login
+              </Menu.Item>
+
+          </Menu>
         </ul>
       );
     }
   }
 
   return (
-    <header className="flex-row px-1">
+    <header className="flex-row px-1"  style ={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center"
+    }}>
       <h1>
         <Link to="/">
           <span role="img" aria-label="shopping bag">
@@ -43,7 +59,9 @@ function Nav() {
       </h1>
 
       <nav>{showNavigation()}</nav>
+      
     </header>
+    
   );
 }
 
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,9 +3,11 @@ import ProductList from "../components/ProductList";
 import CategoryMenu from "../components/CategoryMenu";
 import Cart from "../components/Cart";
 
+//TODO: remove <Nav/> tag?
 const Home = () => {
   return (
     <div className="container">
+      
       <CategoryMenu />
       <ProductList />
       <Cart />
