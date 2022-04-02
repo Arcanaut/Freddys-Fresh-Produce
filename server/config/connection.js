45594c8a8d3bf498cab74ee87f0e8e3c0d9fd856
Brendan Ahearn <91020994+Arcanaut@users.noreply.github.com>
2022-04-01 23:50:12 -0500

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/freshfreddys'
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/FreddysFreshProduce'
 
 , {
   useNewUrlParser: true,
