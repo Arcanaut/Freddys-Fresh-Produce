d1c752b2c01ae938c5a17efa2619c7b122b4b178
Brendan Ahearn <91020994+Arcanaut@users.noreply.github.com>
2022-04-02 00:01:50 -0500

    Update connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/FreddysFreshProduce'
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://Arcanaut:Hostage343518!@cluster0.uylwl.mongodb.net/Cluster0?retryWrites=true&w=majority'
 
 , {
   useNewUrlParser: true,
